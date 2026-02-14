37b4a25d848c26fe5ca522a78788fdfd6f7f7cca
Bobby Mawhinney <bobby.mawhinney@proton.me>
2026-02-14 14:24:38 -0500

    Change site title from 'My Site' to 'My Wee Site'

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,7 +83,7 @@ const config = {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: 'My Site',
+        title: 'My Wee Site',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
